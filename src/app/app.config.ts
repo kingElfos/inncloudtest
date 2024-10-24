379eee0901ac18866cb844636f43f6299abc7ae3
Brayan cadavid <brayan1cadavid@gmail.com>
2024-10-24 04:01:26 -0500

    @fix:solved bug on animations module

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,13 @@
 import { provideHttpClient} from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideRouter(routes, withComponentInputBinding()),
         provideHttpClient(),
+        provideAnimations()
     ],
 };
